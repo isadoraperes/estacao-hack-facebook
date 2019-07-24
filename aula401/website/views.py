e9e80bd4b0999ffa8fc32b324905e748008972cb
Isadora Peres <isadoraoperes@gmail.com>
2019-07-24 17:31:56 -0300

    subir aula 01 semana 04

---
+++ aula401/website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {
+        
+    }
+    return render(request, 'index.html', context)
+
+def sobre(request):
+    context = {
+        
+    }
+    return render(request, 'sobre.html', context)
